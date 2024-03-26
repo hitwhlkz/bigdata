68e78446067767618ee24b6dd279ff7874bb193a
hitwhlkz <hitwhlkz@gmail.com>
2024-03-26 13:01:32 +0800

    引入vuex，vue router，echarts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import echarts from 'echarts';
+import '../src/assets/css/index.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(echarts)
+  .use(router)
+  .use(store)
+  .mount('#app');
